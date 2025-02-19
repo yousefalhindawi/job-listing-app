32d62a0b062823c804c1135d9e0b0f1711790146
Yousef Alhindawi <100360492+yousefalhindawi@users.noreply.github.com>
2025-02-19 12:54:25 +0300

    fix Footer links

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,24 +1,22 @@
 <template>
-    <footer class="bg-gray-800 text-white p-6 mt-12">
-      <div class="container mx-auto text-center">
-        <p>&copy; 2025 JobFinder. All rights reserved.</p>
-        
-        <!-- Social Links (Optional) -->
-        <div class="mt-4 space-x-6">
-          <NuxtLink to="#" class="hover:text-gray-400">Facebook</NuxtLink>
-          <NuxtLink to="#" class="hover:text-gray-400">Twitter</NuxtLink>
-          <NuxtLink to="#" class="hover:text-gray-400">LinkedIn</NuxtLink>
-        </div>
-        
-        <!-- Legal Links (Optional) -->
-        <div class="mt-2 space-x-6">
-          <NuxtLink to="/" class="hover:text-gray-400">Privacy Policy</NuxtLink>
-          <NuxtLink to="/" class="hover:text-gray-400">Terms of Service</NuxtLink>
-        </div>
+  <footer class="bg-gray-800 text-white p-6 mt-12">
+    <div class="container mx-auto text-center">
+      <p>&copy; {{ new Date().getFullYear() }} JobFinder. All rights reserved.</p>
+
+      <div class="mt-4 space-x-6">
+        <NuxtLink to="https://yousefalhindawi.vercel.app/" :external="true" target="_blank" class="hover:text-gray-400">Portfolio</NuxtLink>
+        <NuxtLink to="https://www.linkedin.com/in/yousef-alhindawi/" :external="true" target="_blank" class="hover:text-gray-400">LinkedIn</NuxtLink>
+        <NuxtLink href="mailto::eng.yousefalhindawi@gmail.com" class="hover:text-gray-400">Email</NuxtLink>
       </div>
-    </footer>
-  </template>
-  
-  <script setup>
-  </script>
-  
+
+      <!-- Legal Links (Optional) -->
+      <div class="mt-2 space-x-6">
+        <NuxtLink to="/" class="hover:text-gray-400">Privacy Policy</NuxtLink>
+        <NuxtLink to="/" class="hover:text-gray-400">Terms of Service</NuxtLink>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script setup>
+</script>
