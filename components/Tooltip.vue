50184187c31e4d4dfe1b1c6ffe7acb87d94f7256
Yousef Alhindawi <100360492+yousefalhindawi@users.noreply.github.com>
2025-02-21 19:03:28 +0300

    fix horizontal scroll

--- components/Tooltip.vue
+++ components/Tooltip.vue
@@ -2,7 +2,7 @@
     <span class="absolute top-1/2 right-2 transform -translate-y-1/2 text-gray-500 group">
         <i class="fas fa-info-circle cursor-pointer"></i>
         <span
-            class="absolute bottom-full mb-2 left-1/2 transform -translate-x-1/2 text-sm bg-gray-700 text-white p-1 rounded opacity-0 group-hover:opacity-100 transition-opacity z-10 whitespace-nowrap">
+            class="absolute bottom-full mb-2 right-1/2 transform text-sm bg-gray-700 text-white p-1 rounded opacity-0 group-hover:opacity-100 transition-opacity z-10 whitespace-nowrap">
             {{ infoText }}
         </span>
     </span>
